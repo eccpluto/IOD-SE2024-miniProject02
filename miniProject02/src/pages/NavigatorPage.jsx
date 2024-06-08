f7db9a401195da0e388f440c7be1998cae6fcb13
eccpluto <alleyes.payday591@passmail.net>
2024-06-09 01:44:53 +1200

    recharts integration example

--- miniProject02/src/pages/NavigatorPage.jsx
+++ miniProject02/src/pages/NavigatorPage.jsx
@@ -1,16 +1,26 @@
 import Container from "@mui/material/Container";
-
+import { LineChart, Line } from 'recharts';
 
 /**
  * 
  * @returns NavigatorPage component.
  */
-function NavigatorPage() {
+function NavigatorPage(props) {
+
+    const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
+        { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
+        { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
+        { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
+        { name: 'Page A', uv: 400, pv: 2400, amt: 2400 }
+    ];
 
     return (
         <div className="NavigatorPage">
             <Container>
                 <h1>NavigatorPage</h1>
+                <LineChart width={400} height={400} data={data}>
+                    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                </LineChart>
             </Container>
         </div>
     )
